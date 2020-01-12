8d0df060a2d2293f8c1230316787e22c3eaf7492
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-01-12 11:06:17 -0800

    Add form validation work with reusable component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,111 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-import Store from "./store";
-import { updateData, addData } from "./store/action";
+import { FormInput } from "./form";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+class LoginPage extends React.Component {
+  state = {
+    user: {
+      username: "",
+      password: ""
+    },
+    errors: {},
+    submitted: false
+  };
 
-  componentDidMount() {
-    this.unsubscribe = Store.subscribe(() => {
-      console.log(Store.getState(), "finalist");
+  handleChange = event => {
+    const { user } = this.state;
+    user[event.target.name] = event.target.value;
+    this.setState({ user });
+  };
+
+  onSubmit = () => {
+    const {
+      user: { username, password }
+    } = this.state;
+    let err = {};
+
+    if (!username) {
+      err.username = "Enter your username!";
+    }
+
+    if (password.length < 8) {
+      err.password = "Password must be at least 8 characters!";
+    }
+
+    this.setState({ errors: err }, () => {
+      if (Object.getOwnPropertyNames(this.state.errors).length === 0) {
+        this.setState({ submitted: true });
+      }
     });
-  }
+  };
 
   render() {
+    const {
+      submitted,
+      errors,
+      user: { username, password }
+    } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button
-            className="App-link"
-            onClick={() => Store.dispatch(updateData())}
-          >
-            Call Api
-          </button>
-          <button
-            className="App-link"
-            onClick={() => Store.dispatch(addData({ name: "sarim", age: 19 }))}
-          >
-            Add Data
-          </button>
-        </header>
-      </div>
+      <React.Fragment>
+        {submitted ? (
+          <p>Welcome onboard, {username}!</p>
+        ) : (
+          <React.Fragment>
+            <h3>Login!</h3>
+            <FormInput
+              label="Username"
+              name="username"
+              type="text"
+              value={username}
+              onChange={this.handleChange}
+              placeholder="Enter username..."
+              error={errors.username}
+              required
+              className="input"
+            />
+
+            <FormInput
+              label="Password"
+              name="password"
+              type="number"
+              value={password}
+              onChange={this.handleChange}
+              placeholder="Enter password..."
+              error={errors.password}
+              className="input"
+              required
+            />
+
+            <Button
+              type="submit"
+              label="Submit"
+              className="button"
+              handleClick={this.onSubmit}
+            />
+          </React.Fragment>
+        )}
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+const Button = props => (
+  <button
+    type={props.type}
+    className={props.className}
+    onClick={props.handleClick}
+  >
+    {props.label}
+  </button>
+);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="box">
+        <LoginPage />
+      </div>
+    );
+  }
+}
